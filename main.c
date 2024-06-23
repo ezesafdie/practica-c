f22eee9e6286fbdc9b9236d2803c48aed038cc8a
Ezequiel Safdie <ezesafdie@gmail.com>
2024-06-23 17:19:09 -0300

    Ejercicio - Lectura de archivo de texto basico

--- main.c
+++ main.c
@@ -1,53 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct
-{
-    char* itemName;
-    int quantity;
-    float price;
-    float amount;
-} Item;
-
-void readItem(Item* item);
-void printItem(Item* item);
-
 int main()
 {
-    Item* item = malloc(sizeof(Item));
-    if(!item)
+    int c;
+    int count;
+
+    FILE* file = fopen("file.txt", "r");
+
+    if(!file)
         return -1;
 
-    item->itemName = (char*) malloc(50 * sizeof(char));
-    if(!item->itemName)
-        return -1;
+    while((c = fgetc(file)) != EOF)
+    {
+        if(c == '\n')
+            count++;
 
-    readItem(item);
+        printf("%c", c);
+    }
 
-    printItem(item);
+    fclose(file);
 
-
-    free(item->itemName);
-    free(item);
+    printf("\n\nCantidad de lineas: %d", count + 1);
 
     return  0;
-}
-
-void readItem(Item* item)
-{
-    printf("Enter the item name: \n");
-    scanf("%s", item->itemName);
-
-    printf("\nEnter the quantity: \n");
-    scanf("%d", &item->quantity);
-
-    printf("\nEnter the price: \n");
-    scanf("%f", &item->price);
-
-    item->amount = item->price * item->quantity;
-}
-
-void printItem(Item* item)
-{
-    printf("Item: %s\nQuantity: %d\nPrice:%.2f\nAmount:%.2f", item->itemName, item->quantity, item->price, item->amount);
 }
