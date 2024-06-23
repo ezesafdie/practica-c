35740c2265372b6f16983ff81536642ad21adbdb
Ezequiel Safdie <ezesafdie@gmail.com>
2024-06-23 19:32:17 -0300

    Ejercicio - Lectura y escritura de archivo de texto

--- main.c
+++ main.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int isLowerCase(char c);
+
 int main()
 {
     int c;
     int count;
 
     FILE* file = fopen("file.txt", "r");
+    FILE* fileToWrite = fopen("temp.txt", "w+");
 
     if(!file)
         return -1;
+
+    if(!fileToWrite)
+    {
+        fclose(file);
+        return -1;
+    }
 
     while((c = fgetc(file)) != EOF)
     {
@@ -17,6 +26,14 @@ int main()
             count++;
 
         printf("%c", c);
+
+        if(isLowerCase(c))
+        {
+            fputc((int)c - 32, fileToWrite);
+        } else
+        {
+            fputc(c, fileToWrite);
+        }
     }
 
     fclose(file);
@@ -24,4 +41,14 @@ int main()
     printf("\n\nCantidad de lineas: %d", count + 1);
 
     return  0;
+}
+
+int isLowerCase(char c)
+{
+    if((int) c >= 97 && (int) c <= 122)
+    {
+        return 1;
+    }
+
+    return 0;
 }
